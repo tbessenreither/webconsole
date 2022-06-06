cf9de0ab9a670fef039dfd4a71132f16c8df4a12
tbessenreither <github@adt-it.de>
2022-06-06 21:06:45 +0200

    feat(ts): timestamp converter;

--- src/components/WebConsoleTools/index.ts
+++ src/components/WebConsoleTools/index.ts
@@ -8,6 +8,7 @@ export default class WebConsoleTools extends WebConsolePlugin {
 
 	onRegister() {
 		this._console.registerCommand('json', this, this.json.bind(this));
+		this._console.registerCommand('ts', this, this.timestamp.bind(this));
 	}
 
 	json(command: WebConsoleCommand) {
@@ -17,11 +18,26 @@ export default class WebConsoleTools extends WebConsolePlugin {
 		this.printLn("\n" + JSON.stringify(jsonData, null, 2), { copy: true });
 	}
 
+	timestamp(command: WebConsoleCommand) {
+		let timestamp = command.subcommands ? parseInt(command.subcommands[0]) : Math.round(Date.now() / 1000);
+		this.printLn(`Timestamp converter`, { class: 'info subtitle' });
+		this.printLn(`Unix timestamp: <span data-copy>${timestamp}</span>`, { html: true });
+		this.printLn(`Date: <span data-copy>${new Date(timestamp * 1000).toLocaleString()}</span>`, { html: true });
+		console.log({
+			unix: timestamp,
+			date: new Date(timestamp * 1000),
+		});
+
+	}
+
 	help(command: WebConsoleCommand) {
 		this.printLn(`Help for ${command.subcommands[0]}:`, { class: 'info subtitle' });
 		if (command.subcommands[0] === 'json') {
 			this.printLn('Converts a JSON string to a JSON object, logs it in the console and prints it nicely formated ready to copy.');
-			this.printLn(`<span data-command='json [1, 2, 3, 4, "string"]'">example: [1, 2, 3, 4, "string"]</span>`, { html: true });
+			this.printLn(`<span data-command='json [1, 2, 3, 4, "string"]'>example: [1, 2, 3, 4, "string"]</span>`, { html: true });
+		} else if (command.subcommands[0] === 'ts') {
+			this.printLn('Converts a Unix Timestamp to a readable date string, logs it in the console and prints it nicely formated ready to copy.');
+			this.printLn(`example: <span data-command='ts'>ts</span>`, { html: true });
 		} else {
 			this.printLn(`I don't know anything about the ${command.subcommands[0]} command.`);
 		}
